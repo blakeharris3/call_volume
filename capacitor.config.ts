d966e3f8bb0b5a10682b25f13bb2b1aa3375e0d5
blakeharris3 <harrisblake3@gmail.com>
2024-06-20 05:30:04 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Call Volume',
+  webDir: 'dist'
+};
+
+export default config;
